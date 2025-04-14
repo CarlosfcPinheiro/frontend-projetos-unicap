a06ed9027a98901fc53517e5a35bbfc42f7dfedc
Carlos Pinheiro <79546952+CarlosfcPinheiro@users.noreply.github.com>
2025-04-14 23:59:33 +0000

    feat: Adicionar atividade next

---
+++ meconversor/app/page.js
@@ -0,0 +1,17 @@
+import Link from 'next';
+
+export default function Home() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+      <main>
+        <h1>Seja Bem-vindo!</h1>
+        <p>Essa página tem por objetivo compartilhar um pouco sobre a </p>
+      </main>
+    </>
+  );
+}
