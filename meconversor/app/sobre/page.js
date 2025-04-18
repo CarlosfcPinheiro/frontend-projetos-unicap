99cd3e60efdb90c222d8e8bb78c7339f47aa4522
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-04-18 00:50:29 -0300

    feat: finish sobre page

--- meconversor/app/sobre/page.js
+++ meconversor/app/sobre/page.js
@@ -4,7 +4,17 @@ export default function Sobre() {
     return (
         <div className="sobre__container w-[100%] h-[100vh] flex flex-col items-center gap-5">
             <Navbar />
-            <h1>Sobre</h1>
+            <main className="text-left ml-12 mt-32">
+                <h1 className="font-bold text-6xl">Sobre</h1>
+                <p className="mt-4 w-[50%]">
+                    Olá, sou Carlos Pinheiro, e sou o desenvolvedor desse
+                    projeto em Next. Atualmente estou cursando Sistemas para
+                    Internet na UNICAP e almejo seguir uma carreira acadêmica no
+                    ramo da tecnlogia. Minhas principais tecnologias de
+                    desenvolvimento atualmente são Java, com o framework Spring,
+                    e Javascript com NodeJS.
+                </p>
+            </main>
         </div>
     );
 }
